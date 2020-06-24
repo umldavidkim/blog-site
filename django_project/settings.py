55e83e6ca2d4e06e4763d73c54c6ab4191f31885
David <umldavidkim@gmail.com>
2020-06-24 18:13:12 -0400

    Updated STATIC_ROOT

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# heroku puts static files in staticfiles
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
