47e1658952a5a48637c48c699797c6f28c7700b6
David <umldavidkim@gmail.com>
2020-06-24 18:37:36 -0400

    Updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o^$t5h*sz8ayb5!r0h5#ibb_z%6_nu$wvpg*w4mckbvua6d=b8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myepicdjangoapp.herokuapp.com']
 
 
 # Application definition
