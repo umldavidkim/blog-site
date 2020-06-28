274d560acc060b66420a7ea0db98f8c24a61d28b
David <umldavidkim@gmail.com>
2020-06-28 04:20:04 -0400

    Added profile info to landing page if user is logged in.

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
+
 ALLOWED_HOSTS = ['myepicdjangoapp.herokuapp.com']
 
 
